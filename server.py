68d8446c69ef2ff0e954f484c09bf861019ec9d7
harrisafoster <harrisafoster@gmail.com>
2022-01-03 16:05:58 +0100

    commit initial

---
+++ server.py
@@ -0,0 +1,61 @@
+import json
+from flask import Flask, render_template, request, redirect, flash, url_for
+
+
+def load_clubs():
+    with open('clubs.json') as c:
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
+
+
+def load_competitions():
+    with open('competitions.json') as comps:
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
+
+
+app = Flask(__name__)
+app.secret_key = 'something_special'
+
+competitions = load_competitions()
+clubs = load_clubs()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/show_summary', methods=['POST'])
+def show_summary():
+    club = [club for club in clubs if club['email'] == request.form['email']][0]
+    return render_template('welcome.html', club=club, competitions=competitions)
+
+
+@app.route('/book/<competition>/<club>')
+def book(competition, club):
+    foundClub = [c for c in clubs if c['name'] == club][0]
+    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+    if foundClub and foundCompetition:
+        return render_template('booking.html', club=foundClub, competition=foundCompetition)
+    else:
+        flash("Something went wrong-please try again")
+        return render_template('welcome.html', club=club, competitions=competitions)
+
+
+@app.route('/purchase_places', methods=['POST'])
+def purchase_places():
+    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+    club = [c for c in clubs if c['name'] == request.form['club']][0]
+    placesRequired = int(request.form['places'])
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+    flash('Great-booking complete!')
+    return render_template('welcome.html', club=club, competitions=competitions)
+
+
+# TODO: Add route for points display
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))

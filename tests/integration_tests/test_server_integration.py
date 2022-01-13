4c2cbab602a8dd44ab5e0f071e7c6952742f47e2
harrisafoster <harrisafoster@gmail.com>
2022-01-13 16:56:18 +0100

    Changements des noms des tests (plus parlants maintenant), ajout des vérifications des status_codes

--- tests/integration_tests/test_server_integration.py
+++ tests/integration_tests/test_server_integration.py
@@ -1,7 +1,7 @@
 from server import app
 
 
-def test_not_enough_points():
+def test_user_cannot_book_more_than_points_available():
     app.test_client().get('/')
     app.test_client().post('/show_summary', data=dict(email="john@simplylift.co", ))
     app.test_client().get('/book/Fall%20Classic/Simply%20Lift')
@@ -9,27 +9,31 @@ def test_not_enough_points():
                                       data=dict(places=50, competition='Fall Classic', club='Simply Lift'))
     assert "You don't have enough points." in str(response.data)
     assert "Points available: 13" in str(response.data)
+    assert response.status_code == 200
 
 
-def test_overbook():
+def test_user_cannot_book_more_than_12():
     app.test_client().get('/')
     app.test_client().post('/show_summary', data=dict(email="john@simplylift.co", ))
     app.test_client().get('/book/Fall%20Classic/Simply%20Lift')
     response = app.test_client().post('/purchase_places',
                                       data=dict(places=13, competition='Fall Classic', club='Simply Lift'))
     assert "You cannot book more than 12 places." in str(response.data)
+    assert response.status_code == 200
 
 
-def test_point_change_reflection():
+def test_point_changes_are_reflected():
     app.test_client().get('/')
     app.test_client().post('/show_summary', data=dict(email="john@simplylift.co", ))
     app.test_client().get('/book/Fall%20Classic/Simply%20Lift')
     response = app.test_client().post('/purchase_places', data=dict(places=12, competition='Fall Classic', club='Simply Lift'))
-    assert b'Points available: 13' not in response.data
+    assert b'Points available: 1' in response.data
+    assert response.status_code == 200
 
 
-def test_book_past_event():
+def test_user_cannot_book_past_event():
     app.test_client().get('/')
     app.test_client().post('/show_summary', data=dict(email="john@simplylift.co", ))
     response = app.test_client().get('/book/Spring%20Festival/Simply%20Lift')
     assert b'How many places?' not in response.data
+    assert response.status_code != 500
--- tests/unit_tests/test_server_unit.py
+++ tests/unit_tests/test_server_unit.py
@@ -1,13 +1,13 @@
 from server import app
 
 
-def test_connection_invalid():
+def test_connection_with_unregistered_email():
     response = app.test_client().post('/show_summary', data=dict(email='test@test.com', ))
     assert "Sorry, that email is not registered." in str(response.data)
     assert response.status_code == 200
 
 
-def test_connection_valid():
+def test_connection_with_registered_email():
     response = app.test_client().post('/show_summary', data=dict(email="john@simplylift.co", ))
     assert "Sorry, that email is not registered." not in str(response.data)
     assert response.status_code == 200
@@ -22,4 +22,5 @@ def test_logout():
 
 def test_points_display():
     response = app.test_client().get('/points')
+    assert response.status_code == 200
     assert 'Simply Lift' in str(response.data)
